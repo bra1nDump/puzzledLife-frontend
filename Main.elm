a17d472392a037f646f0d82f7f2b00941b7d6dd7
Kirill Dubovitskiy <kirill2003de@gmail.com>
2018-07-12 14:27:33 -0700

    added index.html to host the elm application. This allowed to add a feature of displaying images of various sizes. The problem was the canvas that has to be of a fixed size and the Elm architecture (one cannot access the properties of the rendered page

--- Main.elm
+++ Main.elm
@@ -1,11 +1,13 @@
+port module Main exposing (main)
+
 import Css exposing (..)
-import Css.Colors exposing(..)
+import Css.Colors exposing (..)
 import Html
 import Html.Styled exposing (..)
 import Html.Styled.Attributes as Attributes exposing (css, href, src)
 
 import Canvas exposing (Size, Point, Canvas, Style(Color), DrawOp(..))
-import Color.exposing (Color)
+import Color exposing (Color)
 
 import Http
 import Debug
@@ -47,6 +49,7 @@ type alias Frame =
 
 type alias Model =
     { pieceID : String
+    , pieceSize : { width: Int, height: Int }
     , readme : String
     , frame : Frame
     , mdl : Material.Model
@@ -57,7 +60,8 @@ init : String -> (Model, Cmd Msg)
 init firstPiece =
     let
         initModel = Model
-                    (firstPiece)
+                    firstPiece
+                    { width = 0, height = 0 }
                     ""
                     (Frame 0 0 0 0)
                     Material.model
@@ -70,6 +74,7 @@ type Msg =
     | DownMsg (Float, Float)
     | MoveMsg (Float, Float)
     | UpMsg (Float, Float)
+    | DimensionsResponse (Int, Int)
     -- internal
     | Mdl (Material.Msg Msg)
 
@@ -84,32 +89,37 @@ update msg model =
         SubmissionStatus (error) ->
             Debug.log (toString error) (model, Cmd.none)
         GetReadmeStatus (Ok readmeString) ->
-            ({ model | readme = readmeString }, Cmd.none)
+            ({ model | readme = readmeString }, dimensionsRequest <| imageLink model)
         GetReadmeStatus (error) ->
-            Debug.log (toString error) (model, Cmd.none)
+            (model, Cmd.none)
         DownMsg (x, y) ->
-            Debug.log (toString (x,y))
                 ({model | frame =
                       let
                           {x2, y2} = model.frame
                       in Frame (x |> floor) (y |> floor) x2 y2
                  }, Cmd.none)
         MoveMsg (x, y) ->
-            Debug.log (toString (x,y))
                 ({model | frame =
                       let
                           {x1, y1} = model.frame
                       in Frame x1 y1 (x |> floor) (y |> floor)
                  }, Cmd.none)
         UpMsg (x, y) ->
-            Debug.log (toString (x,y))
                 ({model | frame =
                       let
                           {x1, y1} = model.frame
                       in Frame x1 y1 (x |> floor) (y |> floor)
                  }, Cmd.none)
+        DimensionsResponse (w, h) ->
+            Debug.log (toString (w, h))
+            ({ model | pieceSize = { width = w, height = h } },
+                 Cmd.none)
         Mdl (message_) ->
             Material.update Mdl message_ model
+
+imageLink : Model -> String
+imageLink model = baseUrl ++ "/puzzles/gallaxy/"
+                  ++ model.pieceID ++ "/image.jpg"
 
 submitSolution : Model -> Cmd Msg
 submitSolution model =
@@ -138,61 +148,86 @@ relativePos : Pointer.Event -> ( Float, Float )
 relativePos event =
     event.pointer.offsetPos
 
-submissionContainer : Model -> Html Msg
-submissionContainer model =
+
+regions : Model -> DrawOp
+regions model =
     let
         {x1, y1, x2, y2} = model.frame
-        frameWidth = abs (x2 - x1)
-        frameHeight = abs (y2 - y1)
+        frameWidth = x2 - x1
+        frameHeight = y2 - y1
     in
-        div [ css [ display inlineBlock
-                  --, position absolute
-                  , float left
-                  , width (px 300)
-                  , height (px 300)
-                  , backgroundColor blue
-                  ]
-                  , Attributes.fromUnstyled <| Pointer.onDown (relativePos >> DownMsg)
-                  , Attributes.fromUnstyled <| Pointer.onMove (relativePos >> MoveMsg)
-                  , Attributes.fromUnstyled <| Pointer.onUp (relativePos >> UpMsg)
-            ]
-            [
-             img [ css
-                    [ position absolute
-                    ]
-              , src (baseUrl ++ "/puzzles/gallaxy/" ++ model.pieceID ++ "/image.jpg")
-                  ][]
-            , canvas --Canvas.initialize (Size 300 300)
-                  [ css
-                    [ position relative
-                    , top (px <| toFloat x1)
-                    , left (px <| toFloat y1)
-                    , width (px <| toFloat frameWidth)
-                    , height (px <| toFloat frameHeight)
-                    , color blue
-                    , backgroundColor blue
-                    ]
-                  ]
-                  []
-            ]
+        [ rectangle (Point (toFloat x1) (toFloat y1)) frameWidth frameHeight (Color.rgba 0 0 255 0.5)
+        , FillStyle <| Color Color.white
+        ] |> Canvas.batch
+
+rectangle : Point -> Int -> Int -> Color.Color -> DrawOp
+rectangle point width height color =
+    [ BeginPath
+    , Rect point (Size width height)
+    , FillStyle <| Color color
+    , Fill
+    ] |> Canvas.batch
+
+submissionContainer : Model -> Html Msg
+submissionContainer model =
+    div [ css [ display inlineBlock
+              , position relative
+              , float left
+              , width (px <| toFloat model.pieceSize.width)
+              , height (px <| toFloat model.pieceSize.height)
+              , maxWidth (px <| toFloat model.pieceSize.width)
+              , maxHeight (px <| toFloat model.pieceSize.height)
+              , backgroundColor blue
+              ]
+        , Attributes.fromUnstyled <| Pointer.onDown (relativePos >> DownMsg)
+        , Attributes.fromUnstyled <| Pointer.onMove (relativePos >> MoveMsg)
+        , Attributes.fromUnstyled <| Pointer.onUp (relativePos >> UpMsg)
+        ]
+    [
+     img [ css
+           [ position absolute
+           , zIndex (int 1)
+           , backgroundSize contain
+           , maxWidth inherit
+           , maxHeight inherit
+           ]
+         , src (baseUrl ++ "/puzzles/gallaxy/" ++ model.pieceID ++ "/image.jpg")
+         ][]
+    , div
+         [ css
+           [ position absolute
+           , zIndex (int 2)
+           ]
+         ]
+         (if model.pieceSize.width /= 0 then
+             [
+              Canvas.initialize (Size model.pieceSize.width model.pieceSize.height)
+             |> Canvas.draw (regions model)
+             |> Canvas.toHtml []
+             |> fromUnstyled
+             ]
+         else [])
+    ]
 
 view : Model -> Html Msg
 view model =
-        div
-        [ css
-          [ --position relative
-          --, display inlineBlock
-          ]
-        ]
-        [ h1 [] [ text model.pieceID ]
-        , submissionContainer model
-        , Markdown.toHtml [] model.readme |> fromUnstyled
-        , Button.render Mdl [0] model.mdl
-            [ Options.onClick SubmitSolution ]
-            [ Html.text "submit solution" ] |> fromUnstyled
-        ]
+    div
+    []
+    [ h1 [] [ text model.pieceID ]
+    , submissionContainer model
+    , Markdown.toHtml [] model.readme |> fromUnstyled
+    , Button.render Mdl [0] model.mdl
+        [ Options.onClick SubmitSolution ]
+        [ Html.text "submit solution" ] |> fromUnstyled
+    ]
 
 
 subscriptions : Model -> Sub Msg
-subscriptions _ = Sub.none
+subscriptions _ =
+    dimensionsResponse DimensionsResponse
 
+-- we will send request for image dimensions
+-- through this port
+port dimensionsRequest : String -> Cmd msg
+
+port dimensionsResponse : ((Int, Int) -> msg) -> Sub msg
